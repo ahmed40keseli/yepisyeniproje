c3842afdf7edc63cd5eb7389f8c7911e6d815699
ahmed40keseli <krkkseli@gmail.com>
2024-01-16 23:16:39 +0300

    session and sql connection

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const session = require('express-session')
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const MySQLStore = require('express-mysql-session')(session);
 
 
 app.set('view engine', 'pug');
@@ -12,22 +12,65 @@ app.set('views', './views');
 
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
-const sequelize = require('./utility/database');
 const accountRoutes = require('./routes/account');
 
+const sequelize = require('./utility/database');
 
+// const options = {
+// 	host: 'localhost',
+// 	port: 3000,
+// 	user: 'root',
+// 	password: 'Ah123?kkk',
+// 	database: 'node2-app'
+// };
+
+// const connection = mysql.createConnection(options);
+
+// const sessionStore = new MySQLStore(options);
+
+// connection.connect((err) => {
+//     if (err) {
+//       console.error('Error connecting to MySQL:', err);
+//       return;
+//     }
+
+// console.log('Connected to MySQL');
+
+// const sessionStore = new MySQLStore({}
+
+// connection.on('error', (err) => {
+//     console.error('MySQL connection error:', err);
+//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       console.log('Reconnecting to MySQL...');
+//       connection.connect();
+//     } else {
+//       throw err;
+//     }
+//   });
+
+//   app.use(session({
+//     key: 'session_cookie_name',
+//     secret: 'your_secret_key',
+//     store: sessionStore,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24, // 1 day
+//     },
+//   }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({
-    secret:'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 3600000
-    }
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser());
+// app.use(session({
+//     secret:'keyboard cat',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         maxAge: 3600000
+//     },
+//     store : options
+// }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use((req, res, next) => {
 //     User.findOne({ name: 'sadikturan' })
@@ -50,13 +93,16 @@ app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', 'error/404.pug'));
 });
 
+
 Product.belongsTo(Writer, {
     foreignKey: {
         allowNull: false
     }
 });
 
+
 Writer.hasMany(Product);
+
 
 sequelize
     .sync()
@@ -107,3 +153,32 @@ sequelize
 app.listen(3000, () => {
     console.log('listening on port 3000');
 });
+
+// mongoose.connect(ConnectionString)
+//     .then(() => {
+//         console.log('connected to mongodb');
+
+//         User.findOne({ name: 'sadikturan' })
+//             .then(user => {
+//                 if (!user) {
+
+//                     user = new User({
+//                         name: 'sadikturan',
+//                         email: 'email@gmail.com',
+//                         cart: {
+//                             items: []
+//                         }
+//                     });
+//                     return user.save();
+//                 }
+//                 return user;
+//             })
+//             .then(user => {
+//                 console.log(user);
+//                 app.listen(3000);
+//             })
+//             .catch(err => { console.log(err) });
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
